abdc29d02a230fac0e329a8c63ff99b6ef5e3ece
pradeau <pradeaubenoit@outlook.fr>
2026-04-10 09:23:32 +0200

    serve html

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.json({ status: 'Posto server is running!' });
+  res.sendFile(path.join(__dirname, 'posto.html'));
 });
 
 app.post('/generate', async (req, res) => {
