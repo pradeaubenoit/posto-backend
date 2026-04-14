93263d9be9f680259ffcd68cfe56050f50802187
pradeau <pradeaubenoit@outlook.fr>
2026-04-14 16:17:17 +0200

    use pg individual vars

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ cloudinary.config({
 });
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST, port: process.env.PGPORT, database: process.env.PGDATABASE, user: process.env.PGUSER, password: process.env.PGPASSWORD,
   ssl: { rejectUnauthorized: false }
 });
 
